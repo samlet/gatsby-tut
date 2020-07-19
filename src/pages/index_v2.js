8048683506094c7f423b437729ffd84a85cbac1b
Samlet Wu <xiaofei.wu@gmail.com>
2020-07-19 20:19:28 +0800

    init version

--- src/pages/index_v2.js
+++ src/pages/index_v2.js
@@ -5,7 +5,5 @@ export default () => (
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
-    <div><button className="primary-button">Click me</button></div>
   </div>
 )
-
