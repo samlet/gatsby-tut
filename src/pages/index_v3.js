cf6e03ea6fce43c5b6f39aadeacc4e25d8c08af4
Samlet Wu <xiaofei.wu@gmail.com>
2020-07-19 23:50:09 +0800

    add css

--- src/pages/index_v3.js
+++ src/pages/index_v3.js
@@ -1,13 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="Hello Gatsby!" />
+    <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div><button className="primary-button">Click me</button></div>
   </div>
 )
 
